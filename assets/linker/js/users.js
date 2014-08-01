c97a3c1cf0cd5cc6ecc57ea5096fb72c106ca6cc
tshi0912 <tshi0912@gmail.com>
2014-08-01 17:21:24 +0800

    add checkbox

--- assets/linker/js/users.js
+++ assets/linker/js/users.js
@@ -6,7 +6,17 @@ $(function () {
             "url": "/users",
             "type": "GET"
         },
+        order: [
+            [3, 'desc']
+        ],
         "columns": [
+            {
+                render: function(data, type, row){
+                    return '<label><input type="checkbox"></label>';
+                },
+                orderable: false,
+                className: 'check'
+            },
             { "data": "nickName" },
             { "data": "email" },
             { "data": "createdAt" }
